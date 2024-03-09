320056b8f651bfe435a8d20f462deb23a2f45391
Alondra Yamilet Olea Cardenas <158488326+AlondraOleaa@users.noreply.github.com>
2024-03-09 21:55:54 +0000

    finalizado

--- miPrincipal/AppAlfabeto.java
+++ miPrincipal/AppAlfabeto.java
@@ -1,5 +1,24 @@
 package miPrincipal;
 
 public class AppAlfabeto {
+    public static void main(String[] args) {
+        System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("              ALFABETO           ");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        metodoA('Z');
+        System.out.println(" ");
+
+    }
+
+    static void metodoA(char c){
+        if(c > 'A')
+            metodoB(c);
+            System.out.println(c);
+    }
+
+    static void metodoB(char c){
+        metodoA(--c);
+    }
     
 }
